3349b2463f03b806131ac18e8b99433e772667a0
moalani <mykjalani@gmail.com>
2021-07-01 18:28:49 -0700

    updated socket_server class to work with test interface

--- Async_TCP_Server/Socket_server.h
+++ Async_TCP_Server/Socket_server.h
@@ -7,7 +7,9 @@
 #include <mutex>
 #include <vector>
 #include "boost/asio.hpp"
+#include "boost/thread/thread.hpp"
 #include "Socket_server_delegate.h"
+#include "boost/enable_shared_from_this.hpp"
 
 
 namespace nbs
@@ -15,12 +17,54 @@ namespace nbs
 
 	namespace util
 	{
-
-		/// \brief Handles a simple TCP server
-		class Socket_server
+		class Connection : public boost::enable_shared_from_this<Connection>
 		{
 		public:
-			void async_accept(std::shared_ptr<Socket_server_delegate_handler>& delegate);
+			Connection(boost::asio::io_context& io_context) : connection_socket(io_context) {
+			}
+
+			void start() {
+			
+				read();
+			}
+
+			boost::asio::ip::tcp::socket& socket() {
+			
+				return connection_socket;
+			}
+
+			~Connection() {
+				assert(subscriptions.size() == 0);
+			};
+			static std::shared_ptr<Connection> create(boost::asio::io_context& io_context) {
+
+				return std::shared_ptr<Connection>(new Connection(io_context));
+			}
+		private:
+			void read() {
+			
+				boost::asio::async_read(connection_socket, boost::asio::buffer(message),
+					[this](boost::system::error_code ec, std::size_t length)
+					{
+						if (!ec)
+						{
+							std::cout << message << std::endl;
+						}
+					});
+			}
+
+
+			boost::asio::ip::tcp::socket connection_socket;
+			
+			enum { max_length = 1024 };
+			char message[max_length];
+		};
+
+		/// \brief Handles a simple TCP server
+		class Socket_server : public boost::enable_shared_from_this<Socket_server>
+		{
+		public:
+			void async_accept();
 			/// \brief Creates an asynchonous TCP server
 			///
 			/// The server operates asynchronously. This constructor returns immediately.
@@ -31,7 +75,7 @@ namespace nbs
 			///
 			/// \throw std::exception Error setting up the server
 
-			Socket_server(unsigned int port, std::shared_ptr<Socket_server_delegate_handler>& delegate);
+			Socket_server(unsigned int port, std::shared_ptr<Test_delegate> delegate);
 
 			/// \brief The number of currently connected clients
 			unsigned int connection_count();
@@ -40,19 +84,35 @@ namespace nbs
 			///
 			/// \param data The data
 			void broadcast(const std::vector<std::uint8_t>& data);
-			void send(boost::asio::ip::tcp::socket& socket, const std::vector<std::uint8_t>& data);
+			void send(boost::asio::ip::tcp::socket& sock, const std::vector<std::uint8_t>& data);
 			/// \brief Closes all connections and shuts down the server
-			~Socket_server() {};
+			void do_close()
+			{
+				_socket.close();
+			}
+			void close()
+			{
+				_io_context.post(boost::bind(&Socket_server::do_close, this));
+			}
+			~Socket_server() {
+				close();
+				if (_thread.joinable())
+					_thread.join();
+			};
 			Socket_server(const Socket_server&) = delete;
 			void operator=(const Socket_server&) = delete;
+			
+		private:
+			boost::asio::io_context _io_context;
+			boost::thread _thread;
+			boost::asio::ip::tcp::socket _socket;
 			boost::asio::ip::tcp::acceptor _acceptor;
-			// optional needs c++17 
-			// refer to https://dens.website/tutorials/cpp-asio/async-tcp-server
-			std::optional<boost::asio::ip::tcp::socket> socket;
-			boost::asio::io_context& server_io_context;
-
-
+			//std::vector<boost::shared_ptr<boost::asio::ip::tcp::socket> > connections;
+			std::vector<boost::asio::ip::tcp::socket > connections;
+			
+			mutable std::mutex mu;
 		};
+
 
 
 	} // namespace util
--- Async_TCP_Server/Socket_server_delegate.h
+++ Async_TCP_Server/Socket_server_delegate.h
@@ -1,10 +1,3 @@
-//
-//  Socket_server_delegate.h
-//  socket
-//
-//  Created by Mohammed Al Ani on 6/19/21.
-//
-
 #ifndef Socket_server_delegate_h
 #define Socket_server_delegate_h
 
@@ -17,13 +10,6 @@
 // as described, please contact me. Also contact me any time for assistance if
 // required.
 
-// vcpkg install boost-asio:x86-windows
-#include <cstdint>
-#include <string>
-#include <vector>
-#include <optional>
-#include "boost/asio.hpp"
-#include <boost/enable_shared_from_this.hpp>
 
 namespace nbs
 {
@@ -70,36 +56,26 @@ namespace nbs
 			virtual void error(const std::string& message, bool fatal) = 0;
 			virtual ~Socket_server_delegate() {}
 		};
-		class Socket_server_delegate_handler : public Socket_server_delegate, public std::enable_shared_from_this<Socket_server_delegate_handler> {
-		public:
 
-			Socket_server_delegate_handler(boost::asio::ip::tcp::socket&& socket, boost::asio::io_context& _io_context, unsigned int _port);
-			void new_connection(const std::string& connection_id);
-			void connection_closed(const std::string& connection_id);
-			void data_received(const std::string& connection_id, const std::vector<std::uint8_t>& data);
-			void error(const std::string& message, bool fatal);
-			void start();
-			void handle_write(const boost::system::error_code& /*error*/,
-				size_t /*bytes_transferred*/);
-			void handle_read(const boost::system::error_code& err, size_t bytes_transferred);
-			~Socket_server_delegate_handler() {
-				std::cout << "Socket_server_delegate_handler destructor.\n";
-				assert(subscriptions.size() == 0);
+		class Test_delegate : public Socket_server_delegate
+		{
+		public:
+			virtual void new_connection(const std::string& connection_id) {
+				std::cout << "New connection: " << connection_id << '\n';
 			}
 
-			boost::asio::ip::tcp::socket _socket;
-			boost::asio::io_context& _io_context;
-			unsigned int _port;
-			boost::asio::ip::tcp::endpoint _endpoint;
-			boost::asio::streambuf _streambuf;
-			std::string _connection_id;
-			std::vector<boost::asio::ip::tcp::socket> clients{};
-			enum { max_length = 1024 };
-			char data[max_length];
-			//std::vector<std::uint8_t> message = { 1,2,3,4,5 };
+			virtual void connection_closed(const std::string& connection_id) {
+				std::cout << "Connection closed: " << connection_id << '\n';
+			}
 
+			virtual void data_received(const std::string& connection_id, const std::vector<std::uint8_t>& data) {
+				std::cout << "Data received for " << connection_id << ": " << std::string(data.begin(), data.end()) << '\n';
+			}
+
+			virtual void error(const std::string& message, bool fatal) {
+				std::cout << (fatal ? "Fatal error: " : "Error: ") << message << '\n';
+			}
 		};
-
 
 	} // namespace util
 
