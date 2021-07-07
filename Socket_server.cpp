83ba88768aa19c3be1ceeb41cfe1c7600fe3f3c8
Mohammed Al Ani <mykjalani@gmail.com>
2021-07-06 18:54:46 -0700

    code cleanup, reformat indents and add few comments

--- Socket_server.cpp
+++ Socket_server.cpp
@@ -5,76 +5,79 @@
 namespace nbs
 {
 
-	namespace util
-	{
-		Socket_server::Socket_server(unsigned int port, std::shared_ptr<Test_delegate> delegate) : _io_context(),
-			_acceptor(_io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
-		{
-
-			std::cout << "Server is online..." << std::endl;
-			_delegate = delegate;
-			boost::asio::ip::tcp::resolver resolver(_io_context);
-			boost::asio::ip::tcp::resolver::query query("127.0.0.1", std::to_string(port));
-			boost::asio::ip::tcp::resolver::iterator iterator = resolver.resolve(query);
-			async_accept();
-			run_io_contex();
-		}
-		void Socket_server::run_io_contex()
-		{
-			try
-			{
-				_thread = std::thread(boost::bind(&boost::asio::io_context::run, &_io_context));
-			}
-			catch (std::exception &e)
-			{
-
-				std::cerr << e.what() << '\n';
-			}
-		}
-
-		void Socket_server::async_accept()
-		{
-			boost::shared_ptr<Connection> connection = Connection::create(_io_context);
-			_acceptor.async_accept(connection->socket(), [this, connection](boost::system::error_code ec)
-								   {
-									   if (!ec)
-									   {
-
-										   std::lock_guard<std::mutex> lock(_read_mutex);
-										   connections.push_back(connection);
-										   std::cout << "Connection Successful !" << std::endl;
-										   std::cout << "Current number of connections = " << connection_count() << std::endl;
-										   connection->start(_delegate, connection_count());
-									   }
-									   async_accept();
-								   });
-		}
-
-		void Socket_server::send(boost::asio::ip::tcp::socket &sock, const std::vector<std::uint8_t> &data)
-		{
-
-			boost::asio::async_write(sock, boost::asio::buffer(data),
-									 boost::bind(&Socket_server::handle_write, this,
-												 boost::asio::placeholders::error,
-												 boost::asio::placeholders::bytes_transferred));
-		}
-		void Socket_server::handle_write(const boost::system::error_code & /*error*/,
-										 size_t /*bytes_transferred*/)
-		{
-			std::lock_guard<std::mutex> lock(_write_mutex);
-		}
-
-		unsigned int Socket_server::connection_count()
-		{
-			return static_cast<unsigned int>(connections.size());
-		}
-
-		void Socket_server::broadcast(const std::vector<std::uint8_t> &data)
-		{
-			for (int i = 0; i < connection_count(); i++)
-			{
-				send(connections[i]->socket(), data);
-			}
-		}
-	}
+namespace util
+{
+// socket server constructor takes a Test_delegate and the port and sets server core attributes (io_contex, acceptor).
+Socket_server::Socket_server(unsigned int port, std::shared_ptr<Test_delegate> delegate) : _io_context(),
+_acceptor(_io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+{
+    
+    std::cout << "Server is online..." << std::endl;
+    _delegate = delegate;
+    boost::asio::ip::tcp::resolver resolver(_io_context);
+    boost::asio::ip::tcp::resolver::query query("127.0.0.1", std::to_string(port));
+    boost::asio::ip::tcp::resolver::iterator iterator = resolver.resolve(query);
+    async_accept();
+    run_io_contex();
 }
+// function to run io_context
+void Socket_server::run_io_contex()
+{
+    try
+    {
+        _thread = std::thread(boost::bind(&boost::asio::io_context::run, &_io_context));
+    }
+    catch (std::exception &e)
+    {
+        
+        std::cerr << e.what() << '\n';
+    }
+}
+// function to start accepting connections and add new connections to socket
+void Socket_server::async_accept()
+{
+    boost::shared_ptr<Connection> connection = Connection::create(_io_context);
+    _acceptor.async_accept(connection->socket(), [this, connection](boost::system::error_code ec)
+                           {
+        if (!ec)
+        {
+            
+            std::lock_guard<std::mutex> lock(_read_mutex);
+            connections.push_back(connection);
+            std::cout << "Connection Successful !" << std::endl;
+            std::cout << "Current number of connections = " << connection_count() << std::endl;
+            connection->start(_delegate, connection_count());
+        }
+        async_accept();
+    });
+}
+// function to send data to a specific socket
+void Socket_server::send(boost::asio::ip::tcp::socket &sock, const std::vector<std::uint8_t> &data)
+{
+    
+    boost::asio::async_write(sock, boost::asio::buffer(data),
+                             boost::bind(&Socket_server::handle_write, this,
+                                         boost::asio::placeholders::error,
+                                         boost::asio::placeholders::bytes_transferred));
+}
+// function to handle write
+void Socket_server::handle_write(const boost::system::error_code & /*error*/,
+                                 size_t /*bytes_transferred*/)
+{
+    std::lock_guard<std::mutex> lock(_write_mutex);
+}
+// return the number of connections
+unsigned int Socket_server::connection_count()
+{
+    return static_cast<unsigned int>(connections.size());
+}
+// function to send message to all connections
+void Socket_server::broadcast(const std::vector<std::uint8_t> &data)
+{
+    for (int i = 0; i < connection_count(); i++)
+    {
+        send(connections[i]->socket(), data);
+    }
+}
+} // util
+} // nbs
--- Socket_server_delegate.h
+++ Socket_server_delegate.h
@@ -15,70 +15,70 @@
 namespace nbs
 {
 
-	namespace util
-	{
+namespace util
+{
 
-		/// \brief Handler for Socket_server events
-		///
-		/// \note All methods of this interface must be thread safe.
-		class Socket_server_delegate
-		{
-			//private:
+/// \brief Handler for Socket_server events
+///
+/// \note All methods of this interface must be thread safe.
+class Socket_server_delegate
+{
+    //private:
+    
+    
+public:
+    /// \brief Called when a new connection is made to the server
+    ///
+    /// \param connection_id A string that uniquely identifies this connection in other calls
+    virtual void new_connection(const std::string& connection_id) = 0;
+    
+    /// \brief Called when a connection is lost
+    ///
+    /// \param connection_id A unique identifier provided to new_connection when the connection being
+    /// closed was connected
+    virtual void connection_closed(const std::string& connection_id) = 0;
+    
+    /// \brief Called when data is received from a client
+    ///
+    /// This method is called as soon as new data is available. The amount of data provided in each call is not
+    /// defined, so implementation should make no assumptions about it.
+    ///
+    /// \param connection_id The unique identifier for the connection which sent the data
+    ///
+    /// \param data The data received.
+    virtual void data_received(const std::string& connection_id, const std::vector<std::uint8_t>& data) = 0;
+    
+    /// \brief Called when there is some error
+    ///
+    /// \param message Error message describing the error
+    ///
+    /// \param fatal If true, the server is no longer in a functional state and may not continue
+    /// to handle current or future connections.
+    virtual void error(const std::string& message, bool fatal) = 0;
+    virtual ~Socket_server_delegate() {}
+};
 
+class Test_delegate : public Socket_server_delegate
+{
+public:
+    virtual void new_connection(const std::string& connection_id) {
+        std::cout << "New connection: " << connection_id << '\n';
+    }
+    
+    virtual void connection_closed(const std::string& connection_id) {
+        std::cout << "Connection closed: " << connection_id << '\n';
+    }
+    
+    virtual void data_received(const std::string& connection_id, const std::vector<std::uint8_t>& data) {
+        std::cout << "Data received for " << connection_id << ": " << std::string(data.begin(), data.end()) << '\n';
+    }
+    
+    virtual void error(const std::string& message, bool fatal) {
+        std::cout << (fatal ? "Fatal error: " : "Error: ") << message << '\n';
+    }
+};
 
-		public:
-			/// \brief Called when a new connection is made to the server
-			///
-			/// \param connection_id A string that uniquely identifies this connection in other calls
-			virtual void new_connection(const std::string& connection_id) = 0;
-
-			/// \brief Called when a connection is lost
-			///
-			/// \param connection_id A unique identifier provided to new_connection when the connection being
-			/// closed was connected
-			virtual void connection_closed(const std::string& connection_id) = 0;
-
-			/// \brief Called when data is received from a client
-			///
-			/// This method is called as soon as new data is available. The amount of data provided in each call is not
-			/// defined, so implementation should make no assumptions about it.
-			///
-			/// \param connection_id The unique identifier for the connection which sent the data
-			///
-			/// \param data The data received.
-			virtual void data_received(const std::string& connection_id, const std::vector<std::uint8_t>& data) = 0;
-
-			/// \brief Called when there is some error
-			///
-			/// \param message Error message describing the error
-			///
-			/// \param fatal If true, the server is no longer in a functional state and may not continue
-			/// to handle current or future connections.
-			virtual void error(const std::string& message, bool fatal) = 0;
-			virtual ~Socket_server_delegate() {}
-		};
-
-		class Test_delegate : public Socket_server_delegate
-		{
-		public:
-			virtual void new_connection(const std::string& connection_id) {
-				std::cout << "New connection: " << connection_id << '\n';
-			}
-
-			virtual void connection_closed(const std::string& connection_id) {
-				std::cout << "Connection closed: " << connection_id << '\n';
-			}
-
-			virtual void data_received(const std::string& connection_id, const std::vector<std::uint8_t>& data) {
-				std::cout << "Data received for " << connection_id << ": " << std::string(data.begin(), data.end()) << '\n';
-			}
-
-			virtual void error(const std::string& message, bool fatal) {
-				std::cout << (fatal ? "Fatal error: " : "Error: ") << message << '\n';
-			}
-		};
-
-	} // namespace util
+} // namespace util
 
 } // namespace nbs
 
